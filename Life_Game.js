57486474baac515a02d3bab10b904451118a7bb9
Sacha Gruring <sacha.gruring@ceff.ch>
2022-10-04 16:28:29 +0200

    Life_Game

---
+++ Life_Game.js
@@ -0,0 +1,122 @@
+$(function(){
+    var interval;
+    var nb_cell = 50
+  for (let i = 0; i < nb_cell; i++){
+      $('table').append("<tr></tr>")
+  }
+  $('tr').each(function(){
+      for (let i = 0; i < nb_cell; i++) {
+          $(this).append("<td></td>")
+      }
+  })
+    $('body').append("<form></form>")
+    $('form').append("<input type='submit' value='Commencer' id='launch'/>")
+
+    $('form').on('submit', function (e){
+        e.preventDefault()
+    })
+
+    let animate = false
+    $(document).on('submit', 'form', function(){
+        animate = !animate
+        if (animate) {
+            startLoop()
+        }
+        else {
+        stopLoop()
+        }
+
+
+
+
+    })
+
+    let right_down = false
+    let left_down = false
+
+    $('body').mousedown(function(event){
+        switch (event.which){
+            case 1:
+                left_down = true
+                right_down = false
+                break;
+
+            case 3:
+                right_down = true
+                left_down = false
+                break;
+        }
+    }).mouseup(function(event){
+        switch (event.which){
+            case 1:
+                left_down = false
+                break;
+
+            case 3:
+                right_down = false
+                break;
+        }
+    })
+
+
+        $('td').mouseover(function () {
+            if (left_down) {
+                $(this).addClass("alive")
+            }
+        })
+
+        $('td').mouseover(function () {
+            if (right_down) {
+                $(this).removeClass("alive")
+            }
+        })
+
+    $('td').click(function(){
+        $(this).addClass("alive")
+    })
+    $('td').contextmenu(function(){
+        $(this).removeClass("alive")
+    })
+
+
+    function startLoop() {
+        algorythm()
+        interval = setInterval(algorythm, 100)
+    }
+
+    function stopLoop() {
+        interval = clearInterval(interval)
+    }
+
+    function algorythm () {
+        let table_array = []
+        console.log("test")
+        $('td').each(function (index) {
+            if ($(this).hasClass("alive")) {
+                table_array.push(1)
+            } else {
+                table_array.push(0)
+            }
+
+        })
+
+        $('td').each(function (index) {
+            let nb_cell_alive = 0
+            for (let x = -1; x <= 1; x++) {
+                for (let y = -nb_cell; y <= nb_cell; y += nb_cell) {
+                    if (table_array[index + x + y] == 1) {
+
+                        nb_cell_alive++
+                    }
+                }
+            }
+            if (nb_cell_alive == 3) {
+                $(this).addClass("alive")
+            } else if (nb_cell_alive > 4 || nb_cell_alive < 3) {
+                $(this).removeClass("alive")
+            }
+
+        })
+    }
+
+})
