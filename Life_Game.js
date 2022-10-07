36cba46af0103fa03caaaa12be792aa8cd672689
Sacha Gruring <sacha.gruring@ceff.ch>
2022-10-07 14:46:23 +0200

    life_game update

--- Life_Game.js
+++ Life_Game.js
@@ -1,6 +1,7 @@
 $(function(){
-    var interval;
+    var interval
     var nb_cell = 50
+    var button_pressed = false
   for (let i = 0; i < nb_cell; i++){
       $('table').append("<tr></tr>")
   }
@@ -10,10 +11,32 @@ $(function(){
       }
   })
     $('body').append("<form></form>")
+    $('form').append("<input type='button' value='generate random' id='random'/>")
     $('form').append("<input type='submit' value='Commencer' id='launch'/>")
+
+    $('#launch').css({"height":"30vh", "width":"50vw", "font-size":"10vh", "float":"right"})
+    $('#random').css({"height":"30vh", "width":"50vw", "font-size":"10vh", "float":"right"})
+    $('table').css({"float":"left"})
 
     $('form').on('submit', function (e){
         e.preventDefault()
+        button_pressed = !button_pressed
+        if (button_pressed){
+            $('#launch').attr("value", "Arrêter")
+        }else{
+            $('#launch').attr("value", "Commencer")
+        }
+    })
+
+    $('#random').click(function(){
+        $('td').removeClass("alive")
+        $('td').each(function(){
+            rnd = Math.floor(Math.random() * 100);
+            if (rnd <= 50){
+                $(this).addClass("alive")
+            }
+        })
+
     })
 
     let animate = false
@@ -90,7 +113,6 @@ $(function(){
 
     function algorythm () {
         let table_array = []
-        console.log("test")
         $('td').each(function (index) {
             if ($(this).hasClass("alive")) {
                 table_array.push(1)
@@ -104,10 +126,30 @@ $(function(){
             let nb_cell_alive = 0
             for (let x = -1; x <= 1; x++) {
                 for (let y = -nb_cell; y <= nb_cell; y += nb_cell) {
+                    if (index % nb_cell == 0 && x == -1){
+                        x++
+                    }
+                    else if (index % nb_cell == nb_cell - 1 && x == 1){
+                        x += nb_cell * nb_cell + 1
+                    }
+
                     if (table_array[index + x + y] == 1) {
 
                         nb_cell_alive++
                     }
+
+                }
+            }
+            for (let x = -1; x <= 1; x++){
+                for(let y = -nb_cell; y <= nb_cell; y += nb_cell){
+
+                    if ($(this).hasClass("left_column") && x == -1){
+                        x++
+                    }
+                    if ($(this).hasClass("right_column") && x == 1){
+                        x--
+                    }
+
                 }
             }
             if (nb_cell_alive == 3) {
@@ -120,3 +162,4 @@ $(function(){
     }
 
 })
+
